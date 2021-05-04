83656c65f6e193dee81368f88bc33d9fd77c27c4
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-05-03 20:59:34 -0700

    Update

---
+++ db.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+let host = process.env.host;
+let database = process.env.database;
+let port = process.env.port;
+let username = process.env.mydbusername;
+let password = process.env.password;
+
+
+let connectionString =
+`postgres://${username}:${password}@${host}:${port}/${database}`;
